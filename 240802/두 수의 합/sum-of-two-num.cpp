22a9c1b9a4d22df3684d1528a66c9a7693cb8c82
zzangss <164142490+zzangss@users.noreply.github.com>
2024-08-02 00:39:58 +0900

    Title: 두 수의 합, Time: 79ms, Memory: 6MB, Status: Passed - CodeTree

--- 240802/두 수의 합/sum-of-two-num.cpp
+++ 240802/두 수의 합/sum-of-two-num.cpp
@@ -1,58 +1,28 @@
-#include <iostream>
+#include<iostream>
 #include<unordered_map>
-#include<algorithm>
-#include<vector>
-#define MAX 2000000000
+#define MAX 100000
 
 using namespace std;
 
 int main() {
-    int n, k; cin >> n >> k;
+	int n, k; cin >> n >> k;
 
-    unordered_map<int, int> umap(n);
-    vector<int> v;
-    vector<int> check;
+	int arr[MAX];
+	unordered_map<int, int> umap;
 
-    for (int i = 0; i < n; i++) {
-        int num; cin >> num;
-        v.push_back(num);
-        if (umap.find(num) != umap.end()) {
-            umap[num]++;
-        }
-        else {
-            umap[num] = 1;
-        }
-    }
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
 
-    sort(v.begin(), v.end());
+	int result = 0;
 
-    check.push_back(v[0]);
-    for (int i = 1; i < v.size(); i++) {
-        if (check.back() == v[i]) continue;
-        check.push_back(v[i]);
-    }
+	for (int i = 0; i < n; i++) {
+		int temp = k - arr[i];
+		result += umap[temp];
 
-    int s = 0, e = check.size() - 1;
-    int cnt = 0;
-    while (s <= e) {
-        if (check[s] + check[e] > k) {
-            e--;
-        }
-        else if (check[s] + check[e] < k) {
-            s++;
-        }
-        else {
-            if (s == e) {
-                for (int i = 1; i < umap[check[s]]; i++) {
-                    cnt += i;
-                }
-            }
-            else cnt += umap[check[s]] * umap[check[e]];
-            s++;
-            e--;
-        }
-    }
+		umap[arr[i]]++;
+	}
 
-    cout << cnt;
-    return 0;
+	cout << result;
+	return 0;
 }
