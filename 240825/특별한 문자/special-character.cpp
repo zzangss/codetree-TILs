276540c82e7dec124100af8699bf26997fa608bb
zzangss <164142490+zzangss@users.noreply.github.com>
2024-08-25 22:40:05 +0900

    Title: 특별한 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240825/특별한 문자/special-character.cpp
+++ 240825/특별한 문자/special-character.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<map>
+#include<vector>
 
 using namespace std;
 
@@ -7,14 +8,18 @@ int main() {
     string str; cin >> str;
 
     map<char,int> m;
+    vector<char> v;
 
     for(int i = 0; i<str.size(); i++){
         m[str[i]]++;
+        if(m[str[i]] == 1){
+            v.push_back(str[i]);
+        }
     }
 
-    for(auto i = m.begin(); i != m.end(); i++){
-        if(i -> second == 1) {
-            cout << i-> first;
+    for(int i = 0; i < v.size(); i++){
+        if(m[v[i]] == 1) {
+            cout << v[i];
             return 0;
         }
 
